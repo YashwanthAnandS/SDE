2949e68ed7ce1025fe2b85bd4990da7faf336d27
YashwanthAnandS <yashwanthanands@gmail.com>
2020-11-15 18:49:26 +0530

    SDE

--- src/com/yash/SDE/Main.java
+++ src/com/yash/SDE/Main.java
@@ -1,8 +1,17 @@
 package com.yash.SDE;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int arr[]=new int[4];
+        System.out.println(arr.length);
+
+        ArrayList<Integer> alist=new ArrayList<Integer>(4);
+        System.out.println(alist.size());
+
+
     }
 }
